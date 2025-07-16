8c994a51e427c92549eb2fdcc92f863ab82a43d0
zhangfu <18072150332@163.com>
2025-07-16 23:42:15 +0800

    feat: about社交通过配置文件渲染

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,5 +2,10 @@ module.exports = {
   content: [
     './themes/hxs/layout/**/*.ejs',
     './source/**/*.md'
-  ]
+  ],
+  variants: {
+    extend: {
+      display: ['group-hover'],
+    },
+  },
 }
